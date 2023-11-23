70c4c94b7a70ead88c46f09b44c27acb43bf98fa
Danny-Rodriguez <3039433+Danny-Rodriguez@users.noreply.github.com>
2023-11-22 22:25:35 -0800

    Provided knowledge and hallucincation mitigation

--- intro/app.ts
+++ intro/app.ts
@@ -5,13 +5,37 @@ import OpenAI from "openai";
 
 const openai = new OpenAI();
 
+const knowledge = {
+  NewYork: {
+    stock: {
+      "Nike SB": {
+        "size 9": 1,
+        "size 10": 1,
+        "size 11": 3,
+        "size 12": 4
+      }
+    }
+  },
+  LosAngeles: {
+    stock: {
+      "Nike SB": {
+        "size 13": 4
+      }
+    }
+  }
+};
+
 const response = await openai.chat.completions.create({
   model: "gpt-3.5-turbo",
   temperature: 0,
   messages: [
     {
+      role: "system",
+      content: `You are a the BestShoes chat bot. Here is your entire knowledge. You know nothing but this knowledge: ${JSON.stringify(knowledge)}. Carefully read through the stock a few times and only consider an item to be available if it has at least 1 unit. Pay close attention to the location, and do not confuse the stock of one location with another. This could badly impact the business. If a size does not explicitly appear under a location, do not consider it in stock.`
+    },
+    {
       role: "user",
-      content: "Hey, I'm Danny"
+      content: "Hey, I want to to buy Nike SB shoes size 13. Do you have them in stock in New York?"
     }
   ]
 });
