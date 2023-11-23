2ce40eb264cf1b62c5d8c7dbf2d9fc63a5052c0a
Danny-Rodriguez <3039433+Danny-Rodriguez@users.noreply.github.com>
2023-11-23 12:38:52 -0800

    OpenAI Function Calling

--- intro/app.ts
+++ intro/app.ts
@@ -8,30 +8,71 @@ const openai = new OpenAI();
 const goal = "Renovate a kitchen";
 const numTasks = 5;
 
+const functions: any = {
+  add: (obj: { num1: number; num2: number }) => {
+    const sum = obj.num1 + obj.num2;
+    console.log("Sum is", sum);
+  },
+  subtract: (obj: { num1: number; num2: number }) => {
+    const diff = obj.num1 - obj.num2;
+    console.log("Difference is", diff);
+  }
+};
+
 const response = await openai.chat.completions.create({
   model: "gpt-3.5-turbo",
   temperature: 0,
   messages: [
     {
       role: "system",
-      content: `You are a talented task planner. The user will tell you their goal and you will generate a list of tasks for them.
-
-      You must repond in JOSN, strictly with the following:
-
-      {
-        tasks: {
-          title: string // Max 100 characters,
-          description: string // Max 120 characters,
-          difficulty: "easy" | "medium" | "hard",
-        }[],
-      }
-      `
+      content: "You are a smart calculator."
     },
     {
       role: "user",
-      content: `Tell me how to achive ${goal}, produce ${numTasks} tasks.`
+      content: `How much is 10 minus 2?`
+    }
+  ],
+  functions: [
+    {
+      name: "add",
+      description: "Adds two numbers",
+      parameters: {
+        type: "object",
+        properties: {
+          num1: {
+            type: "number"
+          },
+          num2: {
+            type: "number"
+          }
+        }
+      }
+    },
+    {
+      name: "subtract",
+      description: "subtracts two numbers",
+      parameters: {
+        type: "object",
+        properties: {
+          num1: {
+            type: "number"
+          },
+          num2: {
+            type: "number"
+          }
+        }
+      }
     }
   ]
 });
 
-console.log(JSON.parse(response.choices[0].message.content as string));
+console.log(response.choices[0]);
+const function_call = response.choices[0].message.function_call;
+
+if (function_call) {
+  const name = function_call.name;
+  const args = JSON.parse(function_call.arguments);
+
+  const fn = functions[name];
+  fn(args);
+}
